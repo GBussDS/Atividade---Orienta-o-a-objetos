9a218ca341c2ee363d94187ccde4898752bcd124
GUILHERME BUSS DA SILVA <B51089@fgv.edu.br>
2023-10-30 17:38:28 -0300

    Aperfeiçoei os try-excepts e adicionei o método checar_estoque.

--- inventario.py
+++ inventario.py
@@ -16,17 +16,35 @@ class Inventario():
             quantidade_atual = 0
         self.estoque[tipo_produto][produto] = quantidade_atual + quantidade_adicionada
 
+    #Remove uma certa quantidade do produto do estoque.
     def vender(self, tipo_produto, produto, quantidade_vendida):
         try:
             quantidade_atual = self.estoque[tipo_produto][produto]
             if quantidade_atual == 0:
-                raise Exception
-        except:
+                raise KeyError
+            if quantidade_atual < quantidade_vendida:
+                raise ValueError
+            self.estoque[tipo_produto][produto] = quantidade_atual - quantidade_vendida
+
+        except KeyError:
             print(f"{produto} está sem estoque.")
-        self.estoque[tipo_produto][produto] = quantidade_atual - quantidade_vendida
+        except ValueError:
+            print("Quantidade vendida é maior que quantidade em estoque.")
     
+    def checar_estoque(self, tipo_produto, produto):
+        try:
+            quantidade_atual = self.estoque[tipo_produto][produto]
+    
+            if quantidade_atual == 0:
+                raise KeyError
+
+            print(f"Ainda restam {quantidade_atual} unidades do produto '{produto}' em estoque.")
+        except KeyError:
+            print(f"{produto} está sem estoque.")
+    
+    #Passa a printar uma linha para cada tipo de produto.
     def __str__(self):
-        inventario = ''
+        inventario = ""
         for tipo_produto in self.estoque:
             inventario += f"{tipo_produto}: {self.estoque[tipo_produto]}\n"
         
@@ -38,5 +56,8 @@ Inventario.criar_estoque("Manga")
 Inventario.criar_estoque("Jogo")
 Inventario.adicionar_estoque("Roupa","Camiseta azul",3)
 Inventario.adicionar_estoque("Roupa","Tênis da nike",5)
-Inventario.adicionar_estoque("Manga","Horimia",5)
+Inventario.checar_estoque("Roupa","Tênis da nike")
+Inventario.vender("Roupa","Tênis da nike",5)
+Inventario.checar_estoque("Roupa","Tênis da nike")
+Inventario.vender("Roupa","Tênis da nike",5)
 print(Inventario)
