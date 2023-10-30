21afc01c353fcccef931fc5b4a7d7d4bc3d89dd7
GUILHERME BUSS DA SILVA <B51089@fgv.edu.br>
2023-10-30 17:17:00 -0300

    Mudei o __str__ do inventário.

--- inventario.py
+++ inventario.py
@@ -4,9 +4,11 @@ class Inventario():
     def __init__(self):
         pass
     
+    #Inicializa um estoque para um certo tipo de produto
     def criar_estoque(self, tipo_produto):
         self.estoque[tipo_produto] = {}
-    
+
+    #Adiciona ao tipo de produto o produto e sua quantidade
     def adicionar_estoque(self, tipo_produto, produto, quantidade_adicionada):
         try:
             quantidade_atual = self.estoque[tipo_produto][produto]
@@ -14,7 +16,7 @@ class Inventario():
             quantidade_atual = 0
         self.estoque[tipo_produto][produto] = quantidade_atual + quantidade_adicionada
 
-    def vender(self, tipo_produto, produto, quantidade_vendida)
+    def vender(self, tipo_produto, produto, quantidade_vendida):
         try:
             quantidade_atual = self.estoque[tipo_produto][produto]
             if quantidade_atual == 0:
@@ -22,9 +24,19 @@ class Inventario():
         except:
             print(f"{produto} está sem estoque.")
         self.estoque[tipo_produto][produto] = quantidade_atual - quantidade_vendida
+    
+    def __str__(self):
+        inventario = ''
+        for tipo_produto in self.estoque:
+            inventario += f"{tipo_produto}: {self.estoque[tipo_produto]}\n"
+        
+        return inventario
 
 Inventario = Inventario()
 Inventario.criar_estoque("Roupa")
+Inventario.criar_estoque("Manga")
+Inventario.criar_estoque("Jogo")
 Inventario.adicionar_estoque("Roupa","Camiseta azul",3)
-Inventario.adicionar_estoque("Roupa","Camiseta azul",5)
-print(Inventario.estoque)
+Inventario.adicionar_estoque("Roupa","Tênis da nike",5)
+Inventario.adicionar_estoque("Manga","Horimia",5)
+print(Inventario)
